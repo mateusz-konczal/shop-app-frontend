4fd3f8ee93e6c1cc835be1fd4a0435c928af9057
Mateusz Kończal <mateuszkonczal96@gmail.com>
2023-03-22 02:20:41 +0100

    Added fullpage layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DefaultModule } from './layouts/default/default.module';
+import { FullpageModule } from './layouts/fullpage/fullpage.module';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { DefaultModule } from './layouts/default/default.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    DefaultModule
+    DefaultModule,
+    FullpageModule
   ],
   providers: [],
   bootstrap: [AppComponent]
