0aa6353f8c09cc1674019115dcd702acb0b1d387
Mateusz Kończal <mateuszkonczal96@gmail.com>
2023-03-28 17:50:20 +0200

    Added the ability to upload images to products

--- src/app/modules/admin/admin-product-update/admin-product-update.component.ts
+++ src/app/modules/admin/admin-product-update/admin-product-update.component.ts
@@ -14,6 +14,9 @@ import { AdminProductUpdate } from './model/adminProductUpdate';
 export class AdminProductUpdateComponent implements OnInit {
 
   productForm!: FormGroup;
+  imageForm!: FormGroup;
+  requiredFileTypes: string = "image/jpeg, image/png";
+  image: string | null = null;
 
   constructor(
     private router: ActivatedRoute,
@@ -25,12 +28,17 @@ export class AdminProductUpdateComponent implements OnInit {
 
   ngOnInit(): void {
     this.getProduct();
+
     this.productForm = this.formBuilder.group({
       name: ['', [Validators.required, Validators.minLength(4)]],
       description: ['', [Validators.required, Validators.minLength(4)]],
       category: ['', [Validators.required, Validators.minLength(4)]],
       price: ['', [Validators.required, Validators.min(0)]],
       currency: ['PLN', Validators.required]
+    });
+
+    this.imageForm = this.formBuilder.group({
+      file: ['']
     });
   }
 
@@ -43,24 +51,45 @@ export class AdminProductUpdateComponent implements OnInit {
 
   submit() {
     let id = Number(this.router.snapshot.params['id']);
-    this.adminProductUpdateService
-      .saveProduct(id, this.productForm.value as AdminProductUpdate)
-      .subscribe({
-        next: product => {
-          this.mapFormValues(product);
-          this.snackBar.open("Produkt został zaktualizowany", '', { duration: 3000 });
-        },
-        error: err => this.adminMessageService.addSpringErrors(err.error)
+    this.adminProductUpdateService.saveProduct(id, {
+      name: this.productForm.get('name')?.value,
+      description: this.productForm.get('description')?.value,
+      category: this.productForm.get('category')?.value,
+      price: this.productForm.get('price')?.value,
+      currency: this.productForm.get('currency')?.value,
+      image: this.image
+    } as AdminProductUpdate).subscribe({
+      next: product => {
+        this.mapFormValues(product);
+        this.snackBar.open("Produkt został zaktualizowany", '', { duration: 3000 });
+      },
+      error: err => this.adminMessageService.addSpringErrors(err.error)
+    });
+  }
+
+  uploadFile() {
+    let formData = new FormData();
+    formData.append('file', this.imageForm.get('file')?.value);
+    this.adminProductUpdateService.uploadImage(formData)
+      .subscribe(result => this.image = result.filename);
+  }
+
+  onFileChange(event: any) {
+    if (event.target.files.length > 0) {
+      this.imageForm.patchValue({
+        file: event.target.files[0]
       });
+    }
   }
 
   private mapFormValues(product: AdminProductUpdate): void {
-    return this.productForm.setValue({
+    this.productForm.setValue({
       name: product.name,
       description: product.description,
       category: product.category,
       price: product.price,
       currency: product.currency
     });
+    this.image = product.image;
   }
 }
--- src/app/modules/admin/admin-product-update/admin-product-update.service.ts
+++ src/app/modules/admin/admin-product-update/admin-product-update.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { AdminProductUpdate } from './model/adminProductUpdate';
+import { UploadResponse } from './model/uploadResponse';
 
 @Injectable({
   providedIn: 'root'
@@ -16,5 +17,9 @@ export class AdminProductUpdateService {
 
   saveProduct(id: number, value: AdminProductUpdate): Observable<AdminProductUpdate> {
     return this.http.put<AdminProductUpdate>("/api/admin/products/" + id, value);
+  }
+
+  uploadImage(formData: FormData): Observable<UploadResponse> {
+    return this.http.post<UploadResponse>("/api/admin/products/upload-image", formData);
   }
 }
