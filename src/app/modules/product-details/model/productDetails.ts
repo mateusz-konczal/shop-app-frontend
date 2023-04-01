674f57d7b7871b0fd20866c2f2f239230eb7e5f5
Mateusz Kończal <mateuszkonczal96@gmail.com>
2023-04-01 23:18:39 +0200

    Improved product models for customer

--- src/app/modules/product-details/model/productDetails.ts
+++ src/app/modules/product-details/model/productDetails.ts
@@ -3,7 +3,6 @@ import { Review } from "./review";
 export interface ProductDetails {
     id: number,
     name: string,
-    category: string,
     description: string,
     fullDescription: string,
     price: number,
