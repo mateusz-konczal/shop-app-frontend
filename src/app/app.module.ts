03b6c9b7d5112fdbf1fe0d61b4432cf933588768
Mateusz Kończal <mateuszkonczal96@gmail.com>
2023-05-08 14:12:00 +0200

    Created an interceptor to add token to admin request

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,10 @@ import { DefaultModule } from './layouts/default/default.module';
 import { FullpageModule } from './layouts/fullpage/fullpage.module';
 import { FullpageadminModule } from './layouts/fullpageadmin/fullpageadmin.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HttpClientModule } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { CookieService } from 'ngx-cookie-service';
 import { FullpageadminemptyModule } from './layouts/fullpageadminempty/fullpageadminempty.module';
+import { JwtInterceptor } from './modules/common/interceptor/jwt.interceptor';
 
 @NgModule({
   declarations: [
@@ -25,7 +26,10 @@ import { FullpageadminemptyModule } from './layouts/fullpageadminempty/fullpagea
     BrowserAnimationsModule,
     HttpClientModule
   ],
-  providers: [CookieService],
+  providers: [
+    CookieService,
+    { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/modules/common/interceptor/jwt.interceptor.ts
@@ -0,0 +1,21 @@
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+import { JwtService } from "src/app/modules/common/service/jwt.service";
+
+@Injectable()
+export class JwtInterceptor implements HttpInterceptor {
+
+    constructor(private jwtService: JwtService) { }
+
+    intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+        let token = this.jwtService.getToken();
+        if (req.url.startsWith("/api/admin") && token) {
+            req = req.clone({
+                headers: req.headers.set("Authorization", "Bearer " + token)
+            });
+        }
+
+        return next.handle(req);
+    }
+}
