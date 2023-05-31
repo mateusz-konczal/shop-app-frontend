317b5d871695a57ee669986fb5c340e7a36b3038
Mateusz Kończal <mateuszkonczal96@gmail.com>
2023-05-31 15:07:42 +0200

    Improved backend querying for payment notification

--- src/app/modules/order/order-notification/order-notification.component.ts
+++ src/app/modules/order/order-notification/order-notification.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { OrderService } from '../order.service';
 import { ActivatedRoute } from '@angular/router';
+import { interval, mergeMap, takeUntil, takeWhile, timer } from 'rxjs';
 
 @Component({
   selector: 'app-order-notification',
@@ -23,6 +24,15 @@ export class OrderNotificationComponent implements OnInit {
   getPaymentNotification() {
     let orderHash = this.router.snapshot.params['orderHash'];
     this.orderService.getPaymentNotification(orderHash)
-      .subscribe(paymentNotification => this.isPaid = paymentNotification.paid);
+      .subscribe(paymentNotification => {
+        this.isPaid = paymentNotification.paid;
+        if (this.isPaid === false) {
+          interval(7000).pipe(
+            mergeMap(() => this.orderService.getPaymentNotification(orderHash)),
+            takeUntil(timer(3 * 60 * 1000)),
+            takeWhile(value => value.paid === false, true)
+          ).subscribe(paymentNotification => this.isPaid = paymentNotification.paid);
+        }
+      });
   }
 }
