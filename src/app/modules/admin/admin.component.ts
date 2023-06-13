87bb670e0d106882ae1e83474773addde3f4d37b
Mateusz Kończal <mateuszkonczal96@gmail.com>
2023-06-13 17:28:43 +0200

    Created an admin dashboard with promoted products

--- src/app/modules/admin/admin.component.ts
+++ src/app/modules/admin/admin.component.ts
@@ -1,10 +1,58 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { MatTable } from '@angular/material/table';
+import { AdminProduct } from './common/model/adminProduct';
+import { AdminService } from './admin.service';
+import { AdminConfirmDialogService } from './common/service/admin-confirm-dialog.service';
+import { MatPaginator } from '@angular/material/paginator';
+import { startWith, switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-admin',
   templateUrl: './admin.component.html',
   styleUrls: ['./admin.component.scss']
 })
-export class AdminComponent {
+export class AdminComponent implements AfterViewInit {
 
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatTable) table!: MatTable<any>;
+
+  displayedColumns: string[] = ["image", "id", "name", "price", "actions"];
+  dataSource: AdminProduct[] = [];
+  totalElements: number = 0;
+
+  constructor(
+    private adminService: AdminService,
+    private dialogService: AdminConfirmDialogService
+  ) { }
+
+  ngAfterViewInit(): void {
+    this.paginator.page.pipe(
+      startWith({}),
+      switchMap(() => {
+        return this.adminService.getSaleProducts(this.paginator.pageIndex, this.paginator.pageSize);
+      })
+    ).subscribe(data => {
+      this.dataSource = data.content;
+      this.totalElements = data.totalElements;
+    });
+  }
+
+  confirmDelete(element: AdminProduct) {
+    this.dialogService.openConfirmDialog("Czy na pewno chcesz usunąć ten produkt?")
+      .afterClosed()
+      .subscribe(result => {
+        if (result) {
+          this.adminService.deleteSaleProduct(element.id)
+            .subscribe(() => {
+              this.dataSource.forEach((value, index) => {
+                if (element == value) {
+                  this.dataSource.splice(index, 1);
+                  this.table.renderRows();
+                  this.totalElements--;
+                }
+              });
+            });
+        }
+      });
+  }
 }
---
+++ src/app/modules/admin/admin.service.ts
@@ -0,0 +1,21 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { AdminProduct } from './common/model/adminProduct';
+import { Page } from '../common/model/page';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AdminService {
+
+  constructor(private http: HttpClient) { }
+
+  getSaleProducts(page: number, size: number): Observable<Page<AdminProduct>> {
+    return this.http.get<Page<AdminProduct>>(`/api/admin/dashboard/saleProducts?page=${page}&size=${size}`);
+  }
+
+  deleteSaleProduct(id: number): Observable<void> {
+    return this.http.delete<void>("/api/admin/dashboard/saleProducts/" + id);
+  }
+}
